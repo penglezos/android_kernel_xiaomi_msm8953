6a613e3dbbb75160b7c219917c94c84afd105057
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-27 13:48:07 +0000

    sched, cpuidle: Track cpuidle state index in the scheduler
    The idle-state of each cpu is currently pointed to by rq->idle_state but
    there isn't any information in the struct cpuidle_state that can used to
    look up the idle-state energy model data stored in struct
    sched_group_energy. For this purpose is necessary to store the idle
    state index as well. Ideally, the idle-state data should be unified.
    
    Change-Id: Ib013757dd192b6f6f0be098b3909f42cf2fb723b
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -136,6 +136,9 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	time_end = ktime_get();
 	trace_cpu_idle_rcuidle(PWR_EVENT_EXIT, dev->cpu);
 
+	/* The cpu is no longer idle or about to enter idle. */
+	sched_idle_set_state(NULL, -1);
+
 	if (!cpuidle_state_is_coupled(dev, drv, index))
 		local_irq_enable();
 
