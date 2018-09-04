2c55adc5baf8bb2e2158369e7c687395a5d2946c
penglezos <panagiotisegl@gmail.com>
2018-09-04 20:45:47 +0300

    build.sh: updates, support zip compression

--- build.sh
+++ build.sh
@@ -1,23 +1,57 @@
 #!/bin/bash
 #
 # Copyright © 2018, "penglezos" <panagiotisegl@gmail.com>
+# Thanks to Vipul Jha for zip creator
 # Android Kernel Compilation Script
 #
 
-echo -e "***********************************************"
+tput reset
+echo -e "==============================================="
 echo    "         Compiling Englezos Kernel             "
-echo -e "***********************************************"
+echo -e "==============================================="
+
 LC_ALL=C date +%Y-%m-%d
 date=`date +"%Y%m%d-%H%M"`
 BUILD_START=$(date +"%s")
+KERNEL_DIR=$PWD
+REPACK_DIR=$KERNEL_DIR/zip
+OUT=$KERNEL_DIR/out
+ZIP_NAME="$VERSION"-"$DATE"
+DTB="dtb"
+VERSION="4.0"
+DATE=`date +"%Y%m%d"`
 export ARCH=arm64 && export SUBARCH=arm64
 export CROSS_COMPILE="/home/englezos/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 
+zip()
+{
+		cd $REPACK_DIR
+		mkdir kernel
+		mkdir treble
+		mkdir nontreble
+		cp $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb $REPACK_DIR/nontreble/
+		cp $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb $REPACK_DIR/treble/
+		cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz $REPACK_DIR/kernel/
+		FINAL_ZIP="Englezos-${VERSION}-${DATE}.zip"
+        zip -r9 "${FINAL_ZIP}" *
+		cp *.zip $OUT
+		rm *.zip
+		rm -rf kernel
+		rm -rf treble
+		rm -rf nontreble
+		cd $KERNEL_DIR
+		rm out/arch/arm64/boot/Image.gz
+		#rm out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb
+		#rm arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb
+}
+
+rm -rf out
 mkdir -p out
 make O=out clean
 make O=out mrproper
 make O=out mido_defconfig
 make O=out -j$(nproc --all)
+zip
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
