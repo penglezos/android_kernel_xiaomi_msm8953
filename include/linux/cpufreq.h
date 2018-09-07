6ecacf67fc51a33c9bd6be0b79238604f6115cec
penglezos <panagiotisegl@gmail.com>
2018-09-08 02:10:13 +0300

    Removed: blu_active governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -522,9 +522,6 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
-extern struct cpufreq_governor cpufreq_gov_blu_active;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 /*********************************************************************
