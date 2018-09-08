7bf8b8e8ca8e49186adccba582054ec4ec6fb215
penglezos <panagiotisegl@gmail.com>
2018-09-08 15:14:51 +0300

    build.sh: update version

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Sat Sep 8 15:14:47 +03 2018"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "englezos"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
