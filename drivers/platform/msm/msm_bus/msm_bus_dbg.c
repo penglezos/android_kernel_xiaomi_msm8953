9219fc20bb31f843ba31d20225aff3243f930e15
penglezos <panagiotisegl@gmail.com>
2018-09-16 21:36:17 +0300

    Revert "msm: msm_bus: Remove unnecessary self assignment"
    This reverts commit f6a3826efc7200c8349187314e3dd5cad98caab4.

--- drivers/platform/msm/msm_bus/msm_bus_dbg.c
+++ drivers/platform/msm/msm_bus/msm_bus_dbg.c
@@ -577,6 +577,7 @@ static ssize_t  msm_bus_dbg_update_request_write(struct file *file,
 	list_for_each_entry(cldata, &cl_list, list) {
 		if (strnstr(chid, cldata->pdata->name, cnt)) {
 			found = 1;
+			cldata = cldata;
 			strsep(&chid, " ");
 			if (chid) {
 				ret = kstrtoul(chid, 10, &index);
