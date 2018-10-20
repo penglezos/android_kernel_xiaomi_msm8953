1be17161723d24df7655906498ae8fbf9b793b50
penglezos <panagiotisegl@gmail.com>
2018-10-20 13:11:45 +0300

    Revert "add new pm kernel driver for android powersuspend"
    This reverts commit 9808cfda74dc66d4c6fc6fab33e43a81c4a2d705.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -30,10 +30,6 @@
 #endif
 #include "mdss_livedisplay.h"
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -895,10 +891,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-#ifdef CONFIG_POWERSUSPEND
-       set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
-#endif
-
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1034,12 +1026,6 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
-
-#ifdef CONFIG_POWERSUSPEND
-       set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
-#endif
-
-	display_on = false;
 
 end:
 	/* clear idle state */
