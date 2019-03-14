82686b2dead6345e75d2f436b4d081d1971783c8
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-03-14 19:28:02 +0200

    build.sh: remove not needed code

--- build.sh
+++ build.sh
@@ -36,10 +36,6 @@ rm *.zip
 cd $KERNEL_DIR
 rm zip/Image.gz-dtb
 
-if [ -f out/arch/arm64/boot/"Image.gz-dtb" ]; then
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-else
-echo "$(tput setaf 1) Build Failed. $(tput sgr 0)"
-fi
