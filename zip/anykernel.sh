e3bee398f59480012248d1dc19bd01b7a942da11
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-06-05 21:29:11 +0300

    [SQUASH] Introduce AnyKernel3 changes.

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -1,4 +1,4 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
@@ -8,7 +8,7 @@ kernel.string=Englezos Kernel for Redmi Note 4(X)
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=mido
 device.name2=redmi note 4
 device.name3=Redmi Note 4
@@ -17,20 +17,21 @@ supported.versions=8.1.0 - 9
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
+
 
 ## Treble Check
 is_treble=$(file_getprop /system/build.prop "ro.treble.enabled");
@@ -44,13 +45,10 @@ fi;
 dump_boot;
 
 # begin ramdisk changes
-
-# init.rc
 insert_line init.rc "import /init.englezos.rc" after "import /init.trace.rc" "import /init.englezos.rc";
 
 # end ramdisk changes
 
 write_boot;
-
 ## end install
 
