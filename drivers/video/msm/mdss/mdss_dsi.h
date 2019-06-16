fe90f5c2397372242a175cba142233dcee4290a8
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-06-16 14:15:05 +0300

    Revert "msm: mdss: Reduce display commit latency caused by deep CPU idle"
    This reverts commit 9f2766d8119e7cfd748aa0e9fcf2186084cffed7.

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -19,7 +19,6 @@
 #include <linux/irqreturn.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/gpio.h>
-#include <linux/pm_qos.h>
 
 #include "mdss_panel.h"
 #include "mdss_dsi_cmd.h"
@@ -572,8 +571,6 @@ struct mdss_dsi_ctrl_pdata {
 	wait_queue_head_t wake_waitq;
 	atomic_t disp_is_on;
 	atomic_t needs_wake;
-
-	struct pm_qos_request pm_qos_req;
 };
 
 struct dsi_status_data {
